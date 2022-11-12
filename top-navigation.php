331e0a37bfa1c0ae1f919daee141ba53f91acef0
dlorenzo2000 <deanlorenzo@gmail.com>
2022-11-12 12:49:18 -0600

    Added styling, image logo, and top-nagivation bar

--- top-navigation.php
+++ top-navigation.php
@@ -17,19 +17,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" 
+        href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" 
+        integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" 
+        crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <ul>
-        <li class="top-nav-li"><a href="index.php">Home</a></li>        
-        <li class="top-nav-li"><a href="about.php">About</a></li>
-        <li class="top-nav-li"><a href="contact.">Contact</a></li>
-        
-        <li class="top-nav-li">
-            <form action="#" class="top-nav-search">
-                <input type="text">
-            </form>
-        <a href="search.php"><button>Search</button></a></li>
-    </ul>    
+    <div class="container top-nav">
+        <div class="row top-nav-links align-item-center">
+            <div class="col-sm-6">
+                <a href="index.php">
+                    <img src="images/foodzagram-logo-sm.png" class="top-logo"/>
+                </a>
+            </div>    
+            <div class="col-sm-6">            
+                <ul>
+                    <li class="top-nav-li"><a href="index.php">Home</a></li>        
+                    <li class="top-nav-li"><a href="about.html">About</a></li>
+                    <li class="top-nav-li"><a href="contact.html">Contact</a></li>        
+                    <li class="top-nav-li"><a href="login.php">Login</a></li>        
+                    <li class="top-nav-li">
+                        <form action="#" class="top-nav-search">
+                            <input type="text">
+                        </form>
+                        <a href="search.php"><button class="btn btn-secondary">
+                            Search</button>
+                        </a>
+                    </li>
+                   
+                </ul>
+            </div>
+        </div>        
+    </div>          
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" 
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" 
+        integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" 
+        crossorigin="anonymous"></script>        
 </body>
 </html>
