e2aad7be3683cba5821c419b6468ea31672d38e4
dlorenzo2000 <deanlorenzo@gmail.com>
2022-11-12 15:57:59 -0600

    Created home page for registered users.

---
+++ home.php
@@ -0,0 +1,26 @@
+<?php
+/****************************************** 
+ * Name: Dean Lorenzo
+ * Student number: 0367298
+ * Course: Web Development - 2008 (228566)
+ * Assignment: Project
+ * Created: Nov 12, 2022
+ * Updated: Nov 12, 2022 
+ ******************************************/ 
+
+    //require('connect.php');
+    require('top-navigation.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
