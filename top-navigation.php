1ecfea4487fd2c51d45be9b2fd88fdef1a607be7
dlorenzo2000 <deanlorenzo@gmail.com>
2022-11-12 11:25:49 -0600

    Added nav bar

---
+++ top-navigation.php
@@ -0,0 +1,35 @@
+<?php
+/****************************************** 
+ * Name: Dean Lorenzo
+ * Student number: 0367298
+ * Course: Web Development - 2008 (228566)
+ * Assignment: Project
+ * Created: Nov 12, 2022
+ * Updated: Nov 12, 2022 
+ ******************************************/ 
+
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li class="top-nav-li"><a href="index.php">Home</a></li>        
+        <li class="top-nav-li"><a href="about.php">About</a></li>
+        <li class="top-nav-li"><a href="contact.">Contact</a></li>
+        
+        <li class="top-nav-li">
+            <form action="#" class="top-nav-search">
+                <input type="text">
+            </form>
+        <a href="search.php"><button>Search</button></a></li>
+    </ul>    
+</body>
+</html>
