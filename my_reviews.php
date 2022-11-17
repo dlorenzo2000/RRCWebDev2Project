f0f47e989e407c242af8d97eebc6c0a6af3c36e4
dlorenzo2000 <deanlorenzo@gmail.com>
2022-11-16 19:34:20 -0600

    Inserted many records.

--- my_reviews.php
+++ my_reviews.php
@@ -14,22 +14,18 @@
     require('connect.php'); 
 
     require_once('header.php');
-    $userid =  $_SESSION['userid'];
-
-    // query the db for all the posts
-    $qry = "SELECT * FROM post WHERE userid = $userid";    
+    $userid =  $_SESSION['userid']; 
     
     // query the restaurants that have review posts
     $qryRestaurant = "SELECT * 
                       FROM restaurant 
-                      JOIN post 
-                      WHERE post.restaurantid = restaurant.restaurantid";
-                     
-    $stm = $db->prepare($qry);
+                      INNER JOIN post 
+                      WHERE post.restaurantid = restaurant.restaurantid
+                        AND post.active = 1";
+                      
     $stmRestaurant = $db->prepare($qryRestaurant);
     
-    $stmRestaurant->execute();
-    $stm->execute();
+    $stmRestaurant->execute(); 
 ?>
 
 <!DOCTYPE html>
@@ -50,18 +46,17 @@
             </div>
         </div>     
         <div class="row">              
-            <?php if(($stm->rowCount() > 0) && $stmRestaurant->rowCount() > 0): ?>
+            <?php if(stmRestaurant->rowCount() > 0): ?>
                 <ul>                
-                    <?php while($dat = $stm->fetch()): ?>
-                        <?php $datRestaurant = $stmRestaurant->fetch() ?>                        
+                    <?php while($datRestaurant = $stmRestaurant->fetch()): ?>                        
                         <li>
                             <h5><?= $datRestaurant['restaurant_name'] ?></h5>                            
-                            <h6><?= $dat['post_title'] ?> - <?= $dat['restaurant_rating'] ?>/10</h6>
+                            <h6><?= $datRestaurant['post_title'] ?> - <?= $datRestaurant['restaurant_rating'] ?>/10</h6>
                             <h6>
-                                Posted on <?= date('F d, Y h:m A', strtotime($dat['created_date'])) ?>
-                                <a href="update_review.php?postid=<?= $dat['postid'] ?>">EDIT</a>                                
+                                Posted on <?= date('F d, Y h:m A', strtotime($datRestaurant['created_date'])) ?>
+                                <a href="update_review.php?postid=<?= $datRestaurant['postid'] ?>">EDIT</a>                                
                             </h6>
-                            <p><?= $dat['post_content'] ?></p>   
+                            <p><?= $datRestaurant['post_content'] ?></p>   
                         </li>
                     <?php endwhile ?>
                 </ul>
