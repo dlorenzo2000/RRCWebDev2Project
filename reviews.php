57d0bc747d3ec0c22757202f4bcdc471595bdc9e
dlorenzo2000 <deanlorenzo@gmail.com>
2022-11-16 18:05:09 -0600

    Created a working-version as a different branch.

--- reviews.php
+++ reviews.php
@@ -5,7 +5,7 @@
  * Course: Web Development - 2008 (228566)
  * Assignment: Final Project
  * Created: Nov 13, 2022
- * Updated: Nov 13, 2022 
+ * Updated: Nov 15, 2022 
  * Purpose: Reviews page that contains food blogs written by registered users.
  *****************************************************************************/
 
@@ -13,22 +13,17 @@
 
     require('connect.php');
     require('header.php');
-
-    // query the db for all the posts
-    $qry = "SELECT * FROM post";    
     
     // query the restaurants that have review posts
     $qryRestaurant = "SELECT * 
-                      FROM restaurant 
-                      JOIN post 
-                      WHERE post.restaurantid = restaurant.restaurantid 
-                        AND active = 1";
-                     
-    $stm = $db->prepare($qry);
+                      FROM post 
+                      INNER JOIN restaurant
+                      WHERE post.restaurantid = restaurant.restaurantid                        
+                        AND post.active = 1";
+                      
     $stmRestaurant = $db->prepare($qryRestaurant);
     
     $stmRestaurant->execute();
-    $stm->execute();
 ?>
 
 <!DOCTYPE html>
@@ -48,21 +43,22 @@
                     class="btn btn-secondary">Write a review</button>
             </div>
         </div>     
-        <div class="row">              
-            <?php if(($stm->rowCount() > 0) && $stmRestaurant->rowCount() > 0): ?>
+        <div class="row">      
+            <?php if($stmRestaurant->rowCount() > 0): ?>
                 <ul>                
-                    <?php while($dat = $stm->fetch()): ?>
-                        <?php $datRestaurant = $stmRestaurant->fetch() ?>                        
+                    <?php while ($datRestaurant = $stmRestaurant->fetch()): ?>                        
                         <li>
-                            <h5><?= $datRestaurant['restaurant_name'] ?></h5>                            
-                            <h6><?= $dat['post_title'] ?> - <?= $dat['restaurant_rating'] ?>/10</h6>
-                            <h6>Posted on <?= date('F d, Y h:m A', strtotime($dat['created_date'])) ?></h6>
-                            <p><?= $dat['post_content'] ?></p>        
+                            <h5><?= $datRestaurant['restaurant_name'] = isset($datRestaurant['restaurant_name']) ? 
+                                $datRestaurant['restaurant_name'] : 'data not found' ?></h5>                            
+                            <h6><?= $datRestaurant['post_title'] ?> - <?= $datRestaurant['restaurant_rating'] ?>/10</h6>
+                            <h6>Posted on <?= date('F d, Y h:m A', strtotime($datRestaurant['created_date'])) ?></h6>
+                            <p><?= $datRestaurant['post_content'] ?></p>        
                         </li>
                     <?php endwhile ?>
                 </ul>
             <?php endif ?>
         </div>
     </div>
+    <?php require_once('footer.php'); ?>
 </body>
 </html>
